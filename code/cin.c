8be34488897cc0232cf5d03c927ea0d2b3c354b8
RibozymeR <ribozyme@web.de>
2019-02-23 16:31:02 +0100

    Update CIN

--- code/cin.c
+++ code/cin.c
@@ -1,68 +1,5 @@
-/*Console Input
-    Copyleft (C) 2017 RibozymeR
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <stdio.h>
 #include "cin.h"
-
-const int VK_0 = 0x30;
-const int VK_1 = 0x31;
-const int VK_2 = 0x32;
-const int VK_3 = 0x33;
-const int VK_4 = 0x34;
-const int VK_5 = 0x35;
-const int VK_6 = 0x36;
-const int VK_7 = 0x37;
-const int VK_8 = 0x38;
-const int VK_9 = 0x39;
-const int VK_A = 0x41;
-const int VK_B = 0x42;
-const int VK_C = 0x43;
-const int VK_D = 0x44;
-const int VK_E = 0x45;
-const int VK_F = 0x46;
-const int VK_G = 0x47;
-const int VK_H = 0x48;
-const int VK_I = 0x49;
-const int VK_J = 0x4A;
-const int VK_K = 0x4B;
-const int VK_L = 0x4C;
-const int VK_M = 0x4D;
-const int VK_N = 0x4E;
-const int VK_O = 0x4F;
-const int VK_P = 0x50;
-const int VK_Q = 0x51;
-const int VK_R = 0x52;
-const int VK_S = 0x53;
-const int VK_T = 0x54;
-const int VK_U = 0x55;
-const int VK_V = 0x56;
-const int VK_W = 0x57;
-const int VK_X = 0x58;
-const int VK_Y = 0x59;
-const int VK_Z = 0x5A;
-
-const int CTRL_CAPS = CAPSLOCK_ON;
-const int CTRL_ALT = LEFT_ALT_PRESSED | RIGHT_ALT_PRESSED;
-const int CTRL_CTRL = LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED;
-const int CTRL_SHIFT = SHIFT_PRESSED;
-
-const int LEFT_BUTTON	= FROM_LEFT_1ST_BUTTON_PRESSED;
-const int MID_BUTTON	= FROM_LEFT_2ND_BUTTON_PRESSED;
-const int RIGHT_BUTTON	= RIGHTMOST_BUTTON_PRESSED;
 
 int cinGetKeyCode(char c){
 	if(c >= '0' && c <= '9') return VK_0 + c - '0';
@@ -72,7 +9,6 @@ int cinGetKeyCode(char c){
 }
 
 static HANDLE hstdin;
-static FILE *errstream = NULL;
 static INPUT_RECORD *eventbuffer;
 static int mhc = 0, mmhc = 0, mwhc = 0, khc = 0;
 static MOUSE_HANDLER *mouse_handlers;
@@ -80,19 +16,7 @@ static MOUSE_MOVE_HANDLER *mousem_handlers;
 static MOUSE_WHEEL_HANDLER *mousew_handlers;
 static KEY_HANDLER *key_handlers;
 
-static void err(char *c, int i){
-	fprintf(errstream, c, i);
-	fflush(errstream);
-}
-
-void cinSetCINOutput(FILE *newerr, bool close_old){
-	if(close_old && errstream != NULL) fclose(errstream);
-	errstream = newerr;
-	err("changed CIN Error output\n", 0);
-}
-
 void cinInit(){
-	err("initiating CIN...\n", 0);
 	hstdin = GetStdHandle(STD_INPUT_HANDLE);
 	
 	eventbuffer = malloc(20 * sizeof(INPUT_RECORD));
@@ -100,7 +24,6 @@ void cinInit(){
 	mousem_handlers = malloc(sizeof(MOUSE_MOVE_HANDLER));
 	mousew_handlers = malloc(sizeof(MOUSE_WHEEL_HANDLER));
 	key_handlers = malloc(sizeof(KEY_HANDLER));
-	err("allocated CIN memory\n", 0);
 }
 
 void cinTerminate(void){
@@ -109,7 +32,6 @@ void cinTerminate(void){
 	free(mousew_handlers);
 	free(key_handlers);
 	free(eventbuffer);
-	err("terminated CIN\n", 0);
 }
 
 void cinAddMouseHandler(MOUSE_HANDLER handler){
@@ -157,12 +79,9 @@ void cinPollEvents(void){
 	GetNumberOfConsoleInputEvents(hstdin, &events);
 	if(events > 0){
 		DWORD eventsread = 0;
-		err("number of events: %i, ", events);
 		ReadConsoleInput(hstdin, eventbuffer, events > 20 ? 20 : events, &eventsread);
-		err("read %i\n", eventsread);
 		for(int i = 0; i < eventsread; ++i){
 			if(eventbuffer[i].EventType == KEY_EVENT){
-				err("\tkey event\n", 0);
 				KEY_EVENT_RECORD event = eventbuffer[i].Event.KeyEvent;
 				for(register int i = 0; i < khc; ++i) (*(key_handlers[i]))((bool)event.bKeyDown, event.wVirtualKeyCode, event.uChar.AsciiChar, event.dwControlKeyState);
 			}
@@ -170,17 +89,14 @@ void cinPollEvents(void){
 				MOUSE_EVENT_RECORD event = eventbuffer[i].Event.MouseEvent;
 				register int type = event.dwEventFlags;
 				if(type == 0 || type & DOUBLE_CLICK){
-					err("\tmouse event\n", 0);
 					COORD pos = event.dwMousePosition;
 					for(register int i = 0; i < mhc; ++i) (*(mouse_handlers[i]))(pos.X, pos.Y, event.dwButtonState, event.dwControlKeyState);
 				}
 				else if(type & MOUSE_MOVED){
-					err("\tmouse move event\n", 0);
 					COORD pos = event.dwMousePosition;
 					for(register int i = 0; i < mmhc; ++i) (*(mousem_handlers[i]))(pos.X, pos.Y, event.dwButtonState, event.dwControlKeyState);
 				}
 				else if(type & MOUSE_WHEELED){
-					err("\tmouse wheel event\n", 0);
 					COORD pos = event.dwMousePosition;
 					for(register int i = 0; i < mwhc; ++i) (*(mousew_handlers[i]))(pos.X, pos.Y, (signed short int)(event.dwButtonState >> 16), event.dwControlKeyState);
 				}
--- code/cin.h
+++ code/cin.h
@@ -5,21 +5,70 @@
 #include <windows.h>
 #include <stdbool.h>
 
-extern const int CTRL_CAPS, CTRL_ALT, CTRL_CTRL, CTRL_SHIFT;
-extern const int LEFT_BUTTON, MID_BUTTON, RIGHT_BUTTON;
+///control key constants
+#define CTRL_CAPS CAPSLOCK_ON
+#define CTRL_ALT (LEFT_ALT_PRESSED | RIGHT_ALT_PRESSED)
+#define CTRL_CTRL (LEFT_CTRL_PRESSED | RIGHT_CTRL_PRESSED)
+#define CTRL_SHIFT SHIFT_PRESSED
+
+///button constants
+#define LEFT_BUTTON FROM_LEFT_1ST_BUTTON_PRESSED
+#define RIGHT_BUTTON RIGHTMOST_BUTTON_PRESSED
+#ifdef FIVE_BUTTON_MOUSE
+	#define MID_L_BUTTON FROM_LEFT_2ND_BUTTON_PRESSED
+	#define MID_BUTTON FROM_LEFT_3RD_BUTTON_PRESSED
+	#define MID_R_BUTTON FROM_LEFT_4TH_BUTTON_PRESSED
+#else
+	#define MID_BUTTON FROM_LEFT_2ND_BUTTON_PRESSED
+#endif
 
 //see https://msdn.microsoft.com/de-de/library/windows/desktop/dd375731(v=vs.85).aspx
-extern const int VK_0, VK_1, VK_2, VK_3, VK_4, VK_5, VK_6, VK_7, VK_8, VK_9;
-extern const int VK_A, VK_B, VK_C, VK_D, VK_E, VK_F, VK_G, VK_H, VK_I, VK_J, VK_K, VK_L, VK_M, VK_N, VK_O, VK_P, VK_Q, VK_R, VK_S, VK_T, VK_U, VK_V, VK_W, VK_X, VK_Y, VK_Z;
+///virtual key codes not defined by windows.h
+#define VK_0 0x30
+#define VK_1 0x31
+#define VK_2 0x32
+#define VK_3 0x33
+#define VK_4 0x34
+#define VK_5 0x35
+#define VK_6 0x36
+#define VK_7 0x37
+#define VK_8 0x38
+#define VK_9 0x39
+#define VK_A 0x41
+#define VK_B 0x42
+#define VK_C 0x43
+#define VK_D 0x44
+#define VK_E 0x45
+#define VK_F 0x46
+#define VK_G 0x47
+#define VK_H 0x48
+#define VK_I 0x49
+#define VK_J 0x4A
+#define VK_K 0x4B
+#define VK_L 0x4C
+#define VK_M 0x4D
+#define VK_N 0x4E
+#define VK_O 0x4F
+#define VK_P 0x50
+#define VK_Q 0x51
+#define VK_R 0x52
+#define VK_S 0x53
+#define VK_T 0x54
+#define VK_U 0x55
+#define VK_V 0x56
+#define VK_W 0x57
+#define VK_X 0x58
+#define VK_Y 0x59
+#define VK_Z 0x5A
 extern int cinGetKeyCode(char c);
 
-/**x, y, button, controls*/
+///x, y, button, controls
 typedef void (*MOUSE_HANDLER)(int, int, int, int);
-/**x, y, button, controls*/
+///x, y, button, controls
 typedef void (*MOUSE_MOVE_HANDLER)(int, int, int, int);
-/**x, y, direction, controls*/
+///x, y, direction, controls
 typedef void (*MOUSE_WHEEL_HANDLER)(int, int, int, int);
-/**keydown, keycode, character, controls*/
+///keydown, keycode, character, controls
 typedef void (*KEY_HANDLER)(bool, int, char, int);
 
 extern void cinSetCINOutput(FILE *output, bool close_old);
@@ -28,12 +77,17 @@ extern void cinTerminate(void);
 
 extern void cinAddMouseHandler(MOUSE_HANDLER handler);
 extern void cinRemoveMouseHandler(MOUSE_HANDLER handler);
+
 extern void cinAddMouseMoveHandler(MOUSE_MOVE_HANDLER handler);
 extern void cinRemoveMouseMoveHandler(MOUSE_MOVE_HANDLER handler);
+
 extern void cinAddMouseWheelHandler(MOUSE_WHEEL_HANDLER handler);
 extern void cinRemoveMouseWheelHandler(MOUSE_WHEEL_HANDLER handler);
+
 extern void cinAddKeyHandler(KEY_HANDLER handler);
 extern void cinRemoveKeyHandler(KEY_HANDLER handler);
+
+///gets all waiting events and sends them to the respective handlers
 extern void cinPollEvents(void);
 
 #endif
